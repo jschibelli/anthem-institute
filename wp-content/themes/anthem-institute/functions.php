b5f58395e7d646cf0317bd7d1b102ad41acf4c29
jschibelli03 <jschibelli@kaldesignco.com>
2022-05-02 16:45:40 -0400

    Replaced existing hard coded menus, registered the navigation locations

--- wp-content/themes/anthem-institute/functions.php
+++ wp-content/themes/anthem-institute/functions.php
@@ -22,6 +22,11 @@ add_action('wp_enqueue_scripts', 'institute_files');
 
 function institute_features() {
   add_theme_support('title-tag');
+
+  //Menu Locations
+  register_nav_menu('headerMenuLocation', 'Header Menu Location');
+  register_nav_menu('footerColumnTwo', 'Footer Column Two');
+  register_nav_menu('footerColumnThree', 'Footer Column Three');
 }
 
 add_action('after_setup_theme', 'institute_features');
