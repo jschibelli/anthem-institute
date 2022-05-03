91e06d4980b2314d87d49678327fc214fc4c3016
jschibelli03 <jschibelli@kaldesignco.com>
2022-05-03 14:39:51 -0400

    Added ACF to the events post. Programed the event-archive to hide past events and sort upcoming events by closest date.

--- wp-content/themes/anthem-institute/functions.php
+++ wp-content/themes/anthem-institute/functions.php
@@ -30,3 +30,24 @@ function institute_features() {
 }
 
 add_action('after_setup_theme', 'institute_features');
+
+// This query sorts the events archive by date and displays
+// the order by date closest to today and removes past events.
+function institute_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+        array(
+        'key' => 'event_date',
+        'compare' => '>=',
+        'value' => $today,
+        'type' => 'numeric'
+      )
+    ));
+  }
+}
+
+add_action('pre_get_posts', 'institute_adjust_queries');
